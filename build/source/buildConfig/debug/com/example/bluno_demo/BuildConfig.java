3c5e079daf8a71cd65da88bb8b2b8a5218ed5275
George Redinger <george@georgeredinger.com>
2014-02-01 18:35:53 -0800

    initial

---
+++ build/source/buildConfig/debug/com/example/bluno_demo/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.bluno_demo;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
